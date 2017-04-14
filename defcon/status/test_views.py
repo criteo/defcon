aadd54a238377dcede656c73f1e987cb73ff798f
Corentin Chary <c.chary@criteo.com>
2017-04-14 15:09:22 +0200

    tests: make them work

--- defcon/status/test_views.py
+++
@@ -1,7 +0,0 @@
-from django import test
-
-from defcon.status import views
-
-class ViewTestcase(test.TestCase):
-    def test_component_view_set(self):
-        views.ComponentViewSet()
--- defcon/status/tests.py
+++ defcon/status/tests.py
@@ -1,6 +1,7 @@
 from django import test
 
 from defcon.status import models
+from defcon.status import views
 
 
 class ModelTestCase(test.TestCase):
@@ -8,3 +9,8 @@ class ModelTestCase(test.TestCase):
         p = models.Plugin.objects.create()
         pi = models.PluginInstance.objects.create(plugin=p)
         self.assertEqual(pi.component, None)
+
+
+class ViewTestcase(test.TestCase):
+    def test_component_view_set(self):
+        views.ComponentViewSet()
