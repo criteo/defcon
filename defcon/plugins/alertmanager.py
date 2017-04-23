68844e7ce4b5b862bccc4904f856bb6da130dc41
Corentin Chary <c.chary@criteo.com>
2017-04-23 18:20:54 +0200

    plugins: finish the AlertManager plugin

--- defcon/plugins/alertmanager.py
+++ defcon/plugins/alertmanager.py
@@ -1,6 +1,9 @@
 """DefCon Prometheus Alertmanager plugin."""
 import requests
+import logging
+import jinja2
 
+from django.utils import dateparse
 from defcon.plugins import base
 
 
@@ -8,13 +11,40 @@ class AlertmanagerPlugin(base.Plugin):
     """DefCon Static plugin.
 
     Config:
-    TODO
+    ```python
+    {
+      'api': 'http://alertmanager:9090/api/v1', // Url to root API.
+      'receiver': 'default', // Get alerts rooted to this receiver.
+      'labels': {}, // Get alerts matching these labels.
+      'title_template': '{{ labels.alertname }}',
+      'description_template': '{{ annotations }}',
+      'link_template': '{{ generatorURL }}',
+      'defcon': 'label' or int, // Label to use as defcon or raw int.
+    }
+    ```
     """
 
     def __init__(self, config=None):
         """Create an instance of the plugin."""
         super(AlertmanagerPlugin, self).__init__(config)
-        # TODO: Validate config is not None.
+        if config is None:
+            config = {}
+
+        self.title_template = config.get(
+            'title_template', '{{ labels.alertname }}'
+        )
+        self.description_template = config.get(
+            'description_template',
+            'Labels: {{ labels }}\nAnnotations: {{ annotations }}'
+        )
+        self.link_template = config.get(
+            'link_template', '{{ generatorURL }}')
+        self.receiver = config.get('receiver', None)
+        self.labels = config.get('labels', None)
+
+        if config:
+            self.api_url = config['api'] + '/alerts/groups'
+            self.defcon = config['defcon']
 
     @property
     def short_name(self):
@@ -38,11 +68,69 @@ class AlertmanagerPlugin(base.Plugin):
 
     def statuses(self):
         """Return the generated statuses."""
-        api_url = self._config['api'] + '/alerts/groups'
-        receiver = self._config.get('receiver', None)
-        labels = self._config.get('labels', None)
+        ret = {}
 
-        r = requests.get(api_url)
+        if self._config is None:
+            return ret
+
+        r = requests.get(self.api_url)
         # TODO: parse stuff an generate status
-        # print (r.json())
-        return {}
+        data = r.json().get('data', [])
+        for root_alert in data:
+            for block in root_alert.get('blocks', []):
+                for alert in block.get('alerts', []):
+                    status = self._to_status(root_alert, block, alert)
+                    if status is not None:
+                        ret[status['id']] = status
+
+        return ret
+
+    @staticmethod
+    def match_labels(labels, needed_labels):
+        """Return True if labels matche needed_labels."""
+        logging.debug('Checking is %s matches %s' % (labels, needed_labels))
+        if needed_labels is None:
+            return True
+        for k, v in needed_labels.items():
+            if labels.get(k) != v:
+                return False
+            # TODO: handle regexp.
+        return True
+
+    @staticmethod
+    def render(template, data):
+        """Render a string as a template."""
+        env = jinja2.Environment()
+        return env.from_string(template).render(**data)
+
+    def _to_status(self, root_alert, block, alert):
+        """Return a status or None."""
+        logging.debug('Handling %s' % (alert))
+        if not self.match_labels(root_alert['labels'], self.labels):
+            return None
+        if not self.match_labels(alert['labels'], self.labels):
+            return None
+        if self.receiver is not None:
+            if block['routeOpts']['receiver'] != self.receiver:
+                return None
+        if alert['inhibited'] or alert.get('silenced'):
+            logging.debug('alert is inactived')
+            return None
+
+        # Guess the level.
+        if type(self.defcon) == int:
+            defcon = self.defcon
+        elif callable(self.defcon):
+            defcon = self.defcon(alert)
+        else:
+            defcon = int(alert['labels'][self.defcon])
+
+        status = base.Status(
+            self.render(self.title_template, alert),
+            defcon,
+            self.render(self.link_template, alert),
+            description=self.render(self.description_template, alert),
+            time_start=dateparse.parse_datetime(alert['startsAt']),
+            time_end=dateparse.parse_datetime(alert['endsAt']),
+        )
+        return status
---
+++ defcon/plugins/test_alertmanager.py
@@ -0,0 +1,52 @@
+"""Test alertmanager."""
+import httmock
+import os
+
+from django import test
+
+from defcon.plugins import alertmanager
+
+
+@httmock.urlmatch(netloc=r'(.*\.)?alertmanager\.io$')
+def _alertmanager_mock(url, request):
+    filename = os.path.join(
+        os.path.dirname(__file__),
+        'spec_alertmanager.json')
+    return open(filename).read()
+
+
+class PluginsTests(test.TestCase):
+    """Test the plugins."""
+
+    _API_URL = 'http://foo.alertmanager.io/api/v1/'
+
+    def test_base(self):
+        """Basic test."""
+        p = alertmanager.AlertmanagerPlugin()
+        self.assertFalse(p.statuses())
+
+    def test_all(self):
+        """Test with some settings."""
+        with httmock.HTTMock(_alertmanager_mock):
+            p = alertmanager.AlertmanagerPlugin(
+                {
+                    'api': self._API_URL,
+                    'defcon': lambda _: 3,
+                    'receiver': 'default',
+                }
+            )
+            statuses = sorted(p.statuses().values())
+            status = statuses[0]
+
+            self.assertEqual(status['title'], 'ExampleAlertAlwaysFiring')
+            self.assertEqual(status['defcon'], 3)
+
+    def test_match_labels(self):
+        """Check that we match labels properly."""
+        p = alertmanager.AlertmanagerPlugin()
+
+        labels = {'foo': 'bar', 'bar': 'foo'}
+        self.assertTrue(p.match_labels(labels, {}))
+
+        self.assertTrue(p.match_labels(labels, {'foo': 'bar'}))
+        self.assertFalse(p.match_labels(labels, {'foo': 'foo'}))
