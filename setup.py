5007adfe07fa4be530fc7a56574cc2bdafeef1c4
Corentin Chary <c.chary@criteo.com>
2017-04-25 09:40:11 +0200

    setup: read *requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 from setuptools import find_packages, setup
+from pip.req import parse_requirements
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
@@ -7,12 +8,20 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+install_requires = parse_requirements('requirements.txt', session='')
+install_requires = [str(v.req) for v in install_requires]
+
+test_require = parse_requirements('tests-requirements.txt', session='')
+test_require = [str(v.req) for v in test_require]
+
 setup(
     name='defcon',
     version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     license='GPLv3',
+    install_requires=install_requires,
+    tests_require=test_require,
     description='defcon.',
     long_description=README,
     url='http://github.com/iksaif/defcon',
