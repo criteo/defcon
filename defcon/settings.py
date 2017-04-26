059bc35a85ee0749bf020fdde1c1a593583369af
Corentin Chary <c.chary@criteo.com>
2017-04-26 16:56:24 +0200

    settings: allow everybody to use the API by default

--- defcon/settings.py
+++ defcon/settings.py
@@ -114,7 +114,7 @@ STATIC_URL = '/static/'
 # REST
 REST_FRAMEWORK = {
   'DEFAULT_PERMISSION_CLASSES': [
-    'rest_framework.permissions.IsAdminUser',
+    'rest_framework.permissions.AllowAny',
   ],
   'PAGE_SIZE': 10
 }
