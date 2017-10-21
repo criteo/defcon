88b31771b6c92d249bcc5df8bef6f46aba22e167
Corentin Chary <c.chary@criteo.com>
2017-10-21 19:30:36 +0200

    status: add links to badge and url

--- defcon/status/views.py
+++ defcon/status/views.py
@@ -66,7 +66,8 @@ def index(request):
 def status(request, component_id):
     """Display a specific component."""
     component = shortcuts.get_object_or_404(models.Component, pk=component_id)
-    return {'component': component}
+    base_url =  "{0}://{1}".format(request.scheme, request.get_host())
+    return {'component': component, 'base_url': base_url}
 
 
 @an_decorators.render_to('badge.svg', content_type="image/svg+xml")
