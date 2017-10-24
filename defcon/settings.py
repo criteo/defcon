75f01682015573b9eabcd36cb26c22439ceddf48
Corentin Chary <c.chary@criteo.com>
2017-10-24 11:06:29 +0200

    Revert "Add django-background-tasks: will be used later for forcing refresh"
    This reverts commit c511f2992f39b803a7acb29fc8ab03cc64cd347f.

--- defcon/settings.py
+++ defcon/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
   'django.contrib.sessions',
   'django.contrib.messages',
   'django.contrib.staticfiles',
-  'background_task',
   'bootstrap3',
   'rest_framework',
   'rest_framework_swagger',
