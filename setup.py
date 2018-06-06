1e928459dc2263adc53cef4f530ca93f9a65e378
Corentin Chary <c.chary@criteo.com>
2018-06-06 15:06:56 +0200

    v0.1.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ _TEST_REQUIRE = [l for l in _TESTS_REQUIREMENTS_TXT if "://" not in l]
 
 setup(
     name='defcon-monitoring',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     license='GPLv3',
